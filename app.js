8977b193380e109adb89422d6e74e0eb2c5fc5f4
Ali <anwar.ali75751@yahoo.fr>
2022-03-02 23:45:15 +0100

    Initial

---
+++ app.js
@@ -0,0 +1,29 @@
+import values from "/requiredForNextLevel.js";
+
+const yourLevelInput = document.getElementById('your-level');
+const targetLevelInput = document.getElementById('target-level');
+const runesRequiredInput = document.getElementById('runes-required')
+
+const calculate = () => {
+  let level = parseInt(yourLevelInput.value)
+  let target = parseInt(targetLevelInput.value);
+
+  let levelsRequired = target - level;
+
+  let runesRequired = 0;
+  for (let i = 0; i < levelsRequired; i++) {
+    runesRequired += values[level + i - 1];
+  }
+
+  runesRequiredInput.value = runesRequired;
+}
+
+const handleKeyUp = () => {
+  if ((yourLevelInput.value !== "")
+    && (targetLevelInput.value !== "")) {
+    calculate();
+  }
+}
+
+yourLevelInput.addEventListener('input', handleKeyUp);
+targetLevelInput.addEventListener('input', handleKeyUp);
